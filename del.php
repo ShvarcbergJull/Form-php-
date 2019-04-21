53b1ac99d177778ab69e2dfb4b6cae061d2510ca
ShvarcbergJull <32794912+ShvarcbergJull@users.noreply.github.com>
2019-04-22 00:18:33 +0800

    Update del.php

--- del.php
+++ del.php
@@ -5,6 +5,8 @@
 </head>
 <body>
 	<?php 
+	error_reporting(0);
+	include "script.php";
 	if(empty($_POST['f'])){ 
 		echo ("вы ничего не выбрали"); 
 	} 
@@ -15,11 +17,21 @@
 		for ($i=0;$i<$j;$i++){ 
 			if(!empty($af[$i])) 
 			{
-				echo ($af[$i]." "); 
-				unlink($af[$i]);
+				if (substr($af[$i], -3) == "txt")
+				{
+					echo ($af[$i]."-удаление...\n"); 
+					unlink("formfile/".$af[$i]);
+				}
+				else
+				{
+					echo ($af[$i]."-этот файл не может быть удалён\n");
+				}
 			}
 		} 
-	 	echo "<p>успешно удалены!</p>";
+		if (check($af))
+		{
+	 		echo "<p><b>Файлы успешно удалены!</b></p>";
+	 	}
 	 }
 ?>
 
